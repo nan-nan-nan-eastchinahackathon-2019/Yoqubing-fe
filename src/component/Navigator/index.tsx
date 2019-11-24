d64a9dc78df8249261a60fc83d1874b5089032af
chenghaopeng <i@chper.cn>
2019-11-24 11:37:48 +0800

    改大图标

--- src/component/Navigator/index.tsx
+++ src/component/Navigator/index.tsx
@@ -5,13 +5,16 @@ import { Icon } from "antd";
 
 export default class Navigator extends React.Component<any, any> {
   render() {
+    const iconStyle = {
+      fontSize: "1.5em"
+    };
     return (
       <div className={styles.navigator}>
-        <NavigatorButton href="/" title="首页" icon={<Icon type="home" theme="filled"  />} />
-        <NavigatorButton href="/wall" title="墙墙" icon={<Icon type="message" theme="filled" />} />
-        <NavigatorButton href="/order" title="跑腿" icon={<Icon type="bell" theme="filled" />} />
-        <NavigatorButton href="/resource" title="资源" icon={<Icon type="read" theme="filled" />} />
-        <NavigatorButton href="/user" title="个人" icon={<Icon type="user" />} />
+        <NavigatorButton href="/" title="首页" icon={<Icon type="home" theme="filled" style={iconStyle} />} />
+        <NavigatorButton href="/wall" title="墙墙" icon={<Icon type="message" theme="filled" style={iconStyle} />} />
+        <NavigatorButton href="/order" title="跑腿" icon={<Icon type="bell" theme="filled" style={iconStyle} />} />
+        <NavigatorButton href="/resource" title="资源" icon={<Icon type="read" theme="filled" style={iconStyle} />} />
+        <NavigatorButton href="/user" title="个人" icon={<Icon type="user" style={iconStyle} />} />
       </div>
     );
   }
